a60f5fbb54dabf1b5f55193b7e286ffb9e9b4fe1
athiraqbs <athira.palak@gmail.com>
2017-09-20 02:19:30 -0700

    first commit

---
+++ form.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+<form method="post" action="insert.php">
+	Name:<input type="text" name="fname">
+	DOB:<input type="date" name="fdate">
+	Email:<input type="text" name="femail">
+	Address:<input type="testarea" name="fadd">
+<input type="submit" name="button" value="submit">
+</form>
+
+</body>
+</html>
